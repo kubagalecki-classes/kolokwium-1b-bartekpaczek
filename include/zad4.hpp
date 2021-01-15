169dfc074402035e507f57a5976e6eae5eeae765
Bartek <bartek.paczek@gmail.com>
2021-01-15 09:17:45 +0100

    zad4

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,4 +3,15 @@
 #include <exception>
 #include <vector>
 
-// tutaj funkcja stworzZespol
+std::vector<Artysta> sworzZespol(int n, std::string& s){
+    if(n!=s.size()){
+        throw std::logic_error("Wyjatek w piatek");
+    };
+
+    std::vector<Artysta> zespol(n);
+    for(int i; i<n; i++){
+        Artysta a(s, i+1);
+        zespol[n] = a;
+    };
+    return zespol;
+}
