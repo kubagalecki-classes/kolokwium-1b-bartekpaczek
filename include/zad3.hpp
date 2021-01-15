ce0654cbc2ddd7efb5d8f2421e54001986dbdd65
Bartek <bartek.paczek@gmail.com>
2021-01-15 09:08:37 +0100

    zad3

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -2,4 +2,16 @@
 
 #include "zad1.hpp"
 
-//tutaj szablon ArtystaPlusInstrument
+template<class T>
+class ArtystaPlusInstrument{
+    Artysta a;
+    T i;
+
+    public:
+    ArtystaPlusInstrument(const Artysta& A, const T& I): a(A), i(I){};
+    unsigned koncert(){
+        return i.liczbaUczestnikow(a.getSluchacze());
+    };
+};
+
+
